49a3a640f02b5de8f3aae3149fd799c4fc34d282
陈博洋 <kuzoncby@gmail.com>
2017-02-04 21:27:00 +0800

    room api done

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,7 +15,23 @@ class ChatController extends Controller
      */
     public function rooms(Request $request)
     {
-        $rooms = Auth::user()->rooms()->get();
+        $user = $this->user($request);
+        $rooms = $user->rooms()->get();
         return $rooms;
+    }
+
+    /**
+     * Who authenticated
+     * @param Request $request
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function user(Request $request)
+    {
+        if (Auth::user()) {
+            $user = Auth::user();
+        } else {
+            $user = User::where('email', $request->email)->first();
+        }
+        return $user;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/rooms', 'ChatController@rooms');
+Route::post('/me', 'ChatController@user');
+Route::post('/rooms', 'ChatController@rooms');
